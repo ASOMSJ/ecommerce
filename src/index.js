d44247464b85f89abf717f47e3a0660fc8278ad7
ASOMSJ <bawazirabdulmalik@gmail.com>
2024-01-03 16:58:14 +0300

    done most product & brand section

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ import '@fortawesome/fontawesome-free/js/all.min';
 
 
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
+
+
+
+var buttons = document.querySelectorAll('.add-to-cart-btn');
+buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        alert('أضيف المنتج الى عربة الشراء');
+    });
+});
