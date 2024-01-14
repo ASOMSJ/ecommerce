2962bd74ad972185f1ec5be50904e52ec30bb489
ASOMSJ <bawazirabdulmalik@gmail.com>
2024-01-14 16:42:28 +0300

    add product page

--- webpack.config.js
+++ webpack.config.js
@@ -78,6 +78,11 @@ module.exports = {
             template: "./src/index.html",
 
         }),
+        new HtmlWebpackPlugin({
+            filename: "product.html",
+            template: "./src/product.html",
+
+        }),
         new MiniCssExtractPlugin({
             filename: "css/style.css",
         }),
