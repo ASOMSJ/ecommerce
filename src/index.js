9c622f05c2656c58b22a10f209136348a704f27f
ASOMSJ <bawazirabdulmalik@gmail.com>
2024-01-17 16:08:20 +0300

    product page done

--- src/index.js
+++ src/index.js
@@ -14,5 +14,22 @@ buttons.forEach(function (button) {
         alert('أضيف المنتج الى عربة الشراء');
     });
 });
+document.querySelectorAll('.size-option input[type="radio"]').forEach(item=>{
+    item.addEventListener('change', ()=>{
+        document.querySelectorAll('.size-option').forEach(i=>{
+            i.classList.remove('active')
+        })
+        item.parentNode.parentNode.classList.add('active')
+    });
+});
+document.querySelectorAll('.color-option input[type="radio"]').forEach(item=>{
+    item.addEventListener('change', ()=>{
+        document.querySelectorAll('.color-option').forEach(i=>{
+            i.classList.remove('active')
+        })
+        item.parentNode.parentNode.classList.add('active')
+    });
+});
 
 document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة سنة " + new Date().getFullYear();
+
