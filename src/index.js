52a76d74544ae0319a5cdbaeef5cf59e8c26cb26
ASOMSJ <bawazirabdulmaik@gmail.com>
2024-01-20 22:36:06 +0300

    add dynamic total price

--- src/index.js
+++ src/index.js
@@ -14,22 +14,35 @@ buttons.forEach(function (button) {
         alert('أضيف المنتج الى عربة الشراء');
     });
 });
-document.querySelectorAll('.size-option input[type="radio"]').forEach(item=>{
-    item.addEventListener('change', ()=>{
-        document.querySelectorAll('.size-option').forEach(i=>{
+document.querySelectorAll('.size-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.size-option').forEach(i => {
             i.classList.remove('active')
         })
         item.parentNode.parentNode.classList.add('active')
     });
 });
-document.querySelectorAll('.color-option input[type="radio"]').forEach(item=>{
-    item.addEventListener('change', ()=>{
-        document.querySelectorAll('.color-option').forEach(i=>{
+document.querySelectorAll('.color-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.color-option').forEach(i => {
             i.classList.remove('active')
         })
         item.parentNode.parentNode.classList.add('active')
     });
 });
+
+//حساب سعر اجمالي المنتج
+document.querySelectorAll('[data-product-quantity]').forEach(item => {
+    item.addEventListener('change', () => {
+        const newQuantity = item.value;
+        const parent = item.closest('[data-product-info]');
+        const pricePerUbit = parent.getAttribute('[data-product-price]');
+        const totalpriceforbroduct = newQuantity * pricePerUbit;
+        parent.querySelector('.total-price-for-product').innerHTML= totalpriceforbroduct + "$"
+    });
+
+});
+
 
 document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة سنة " + new Date().getFullYear();
 
